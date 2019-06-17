8ee6062a23d200eb30d6467df10724f0edc7b033
asakura <sample@sample.com>
2019-06-17 19:30:06 +0000

    pictgram

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, length: { in: 1..15 }
   
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, length: { in: 8..32 }, 
+  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]/i }
+      
   has_secure_password
 end
